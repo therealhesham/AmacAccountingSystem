7a3a92ac5016cc46b7e6bb05587ef142617d30a5
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-09-04 13:20:46 +0300

    commit on master

--- src/contractor/contractor.controller.ts
+++ src/contractor/contractor.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {Req,Res,Get,Post, Controller } from '@nestjs/common';
+import { Request,Response } from 'express';
 
+import { ContractorService } from './contractor.service';
+import { Prisma, PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
 @Controller('contractor')
-export class ContractorController {}
+export class ContractorController {
+constructor(  private contractorService : ContractorService ){}
+
+
+@Post("/addinvoice")
+async addinvoice(@Req() req:Request,@Res() res:Response){
+
+
+return this.contractorService.Addinvoice(req,res)
+
+}
+
+@Post("/addpaymnet")
+async addpayment(@Req() req:Request,@Res() res:Response){
+
+
+return this.contractorService.Addpayment(req,res)
+
+}
+
+@Get("/")
+async List(@Req() req:Request,@Res() res:Response){
+
+const li = await prisma.contractor.findMany()
+res.status(200).json(li)
+// return this.contractorService.AddContractor(req,res)
+    
+    }
+
+    
+
+@Post("/addcontractor")
+async addContractor(@Req() req:Request,@Res() res:Response){
+
+
+return this.contractorService.AddContractor(req,res)
+
+}
+
+
+}
--- src/contractor/contractor.service.ts
+++ src/contractor/contractor.service.ts
@@ -1,4 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
 
 @Injectable()
-export class ContractorService {}
+export class ContractorService {
+
+async Addinvoice(req,res){
+const {Amount,InvoiceNO,ContractodID} = req.body;
+try {
+const createInvoice = await prisma.invoices.create({data:{Amount:parseFloat(Amount),InvoiceNO,ContractodID}})
+res.status(200).json(createInvoice)
+} catch (error) {
+res.status(301).json(error)
+    
+}
+
+}
+
+async AddContractor(req,res){
+try {
+    const {Amount,Name,WorkPlace}=req.body;
+
+const create = await prisma.contractor.create({data:{Amount,Name,WorkPlace}})
+
+
+res.status(200).json(create)
+
+} catch (error) {
+res.status(301).json(error)
+    
+}
+}
+
+
+async Addpayment(req,res){
+    try {
+        const {Payment,Name,WorkPlace,id}=req.body;
+    return await prisma.$transaction(async (tx)=>{
+// await tx.contractor.update({where:id,data:{Amount:{decrement:Payment}}})
+
+const t =await tx.payment.create({data:{Payment:parseFloat(Payment),Date:new Date().toLocaleString(),WorkPlace,ContractodID:id}})
+const update = await tx.contractor.update({where:id,data:{Amount:{decrement:Payment}}})
+if(update.Amount < 0)    throw new Error("error updating , funds is greater")
+const doubleentry= await tx.double_Entry.create({data:{CreditAmount:Payment,DebitAmount:Payment,CreditName:Name,DebitName:"خزينة",CreditType:"دفعة"}})
+res.status(200).json(doubleentry)   })
+
+    } catch (error) {
+    res.status(301).json(error)
+        
+    }
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+}
