0ec77f32cedfdaa9894eae023d376fe4b032baf0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-09-15 18:08:18 +0300

    commit on

--- src/legacy-double-entry/legacy-double-entry.service.ts
+++ src/legacy-double-entry/legacy-double-entry.service.ts
@@ -6,22 +6,49 @@ export class LegacyDoubleEntryService {
 
 async MoveTransactions() {
 const findAll = await prisma.double_Entry.findMany()
-   
-const moveToLegacy = await prisma.legacy_Double_Entry.createMany({data:findAll})
-const deleteAll = await prisma.double_Entry.deleteMany()
-const find = await prisma.safe.findMany()
- 
-const waiter =await prisma.safe.create({data:{Quantity:find[find.length-1].Quantity+0,Date:new Date().toLocaleDateString()}})
+   try {
+    const moveToLegacy = await prisma.legacy_Double_Entry.createMany({data:findAll})
+    const deleteAll = await prisma.double_Entry.deleteMany()
+        
+   } catch (error) {
+    console.log(error)
+   }
 
-const findBankAccount = await prisma.safe.findMany()
- try {
+
+   try {
+    
+   } catch (error) {
+    
+   }
+
+   try {
+    const find = await prisma.safe.findMany()
+ 
+    const waiter =await prisma.safe.create({data:{Quantity:find[find.length-1].Quantity+0,Date:new Date().toLocaleDateString()}})
+        
+   } catch (error) {
+    const waiters =await prisma.safe.create({data:{Quantity:0,Date:new Date().toLocaleDateString()}})
+    
+   }
+try {
     const createdate = await prisma.date.findFirst()
   const creator =   await prisma.date.update({where:{id:createdate.id},data:{date:new Date().toLocaleDateString()}})       
 } catch (error) {
      const createdate = await prisma.date.create({data:{date:new Date().toLocaleDateString()}})
     
  }
-const waiterBank =await prisma.bank_Account.create({data:{Quantity:findBankAccount[findBankAccount.length-1].Quantity+0,Date:new Date().toLocaleDateString()}})
+try {
+  const findBankAccount = await prisma.safe.findMany()
+ 
+  const waiterBank =await prisma.bank_Account.create({data:{Quantity:findBankAccount[findBankAccount.length-1].Quantity+0,Date:new Date().toLocaleDateString()}})
+    
+} catch (error) {
+
+  const waiterBank =await prisma.bank_Account.create({data:{Quantity:0,Date:new Date().toLocaleDateString()}})
+    
+  console.log(error)
+  
+}
 console.log("done")
 
 }
